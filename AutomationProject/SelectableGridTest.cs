f7b0fcf22512e25c37b031c6ab3b470f4755e43a
cristinafilipan <127209417+cristinafilipan@users.noreply.github.com>
2025-01-30 16:23:56 +0200

    some improvements added on checking deselection or selection of elements (SelectableGridTest class)

--- AutomationProject/SelectableGridTest.cs
+++ AutomationProject/SelectableGridTest.cs
@@ -1,11 +1,12 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using System.Data;
 
 namespace AutomationProject
 {
     public class SelectableGridTest
     {
-       
+
         IWebDriver Driver;
         [Test]
         public void Test()
@@ -15,12 +16,11 @@ namespace AutomationProject
             Driver.Navigate().GoToUrl("https://demoqa.com/");
             Driver.Manage().Window.Maximize();
             IJavaScriptExecutor js = (IJavaScriptExecutor)Driver;
-           
+
 
             IWebElement interactionsElement = Driver.FindElement(By.XPath("//h5[text()='Interactions']"));
             interactionsElement.Click();
 
-            js.ExecuteScript("window.scrollTo(0,1000)");
 
             IWebElement selectableElement = Driver.FindElement(By.XPath("//span[text()='Selectable']"));
             selectableElement.Click();
@@ -28,16 +28,65 @@ namespace AutomationProject
 
             IWebElement gridElement = Driver.FindElement(By.Id("demo-tab-grid"));
             gridElement.Click();
+            js.ExecuteScript("window.scrollTo(0,1000)");
 
             List<IWebElement> itemsList = Driver.FindElements(By.XPath("//li[@class='list-group-item list-group-item-action']")).ToList();
 
-            for(int i=0; i<itemsList.Count; i+=2 )
+            bool alreadySelected = false;
+            foreach (var item in itemsList)
+            {
+                if (item.GetAttribute("class").Contains("active"))
+                {
+                    alreadySelected = true;
+                    break;
+                }
+            }
+
+
+            if (alreadySelected)
+            {
+                Console.WriteLine("At least one element was already selected at the beginning!");
+            }
+            else
+            {
+                Console.WriteLine("All elements were initially deselected!");
+            }
+
+
+            for (int i = 0; i<itemsList.Count; i+=2)
             {
                 itemsList[i].Click();
                 System.Threading.Thread.Sleep(1000);
             }
-            
-          
+
+            bool selectionSucceded = true;
+            List<string> notSelectedElements = new List<string>();
+
+
+            for (int i = 0; i<itemsList.Count; i+=2)
+            {
+                if (!itemsList[i].GetAttribute("class").Contains("active"))
+                {
+                    selectionSucceded = false;
+                    notSelectedElements.Add(itemsList[i].Text);
+                }
+            }
+
+
+            if (selectionSucceded)
+            {
+                Console.WriteLine("All elements were selected successfully!");
+            }
+            else
+            {
+                Console.WriteLine("The following elements were not selected:");
+                foreach (string item in notSelectedElements)
+                {
+
+                    Console.WriteLine("  "+item);
+                }
+            }
+
         }
     }
 }
